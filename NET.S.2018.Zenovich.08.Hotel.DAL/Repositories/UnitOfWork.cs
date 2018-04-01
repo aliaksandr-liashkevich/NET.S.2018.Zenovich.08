fd79d94f340537267958a1b78e572b57bc5d61e4
azenovich <alexander.zenovich@gmail.com>
2018-04-01 14:11:16 +0300

    UnitOfWork and his API was created.

---
+++ NET.S.2018.Zenovich.08.Hotel.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using NET.S._2018.Zenovich._08.Hotel.DAL.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2018.Zenovich._08.Hotel.DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            CleanUp(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void CleanUp(bool clean)
+        {
+            if (!this.disposed)
+            {
+                if (clean)
+                {
+
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        ~UnitOfWork()
+        {
+            CleanUp(false);
+        }
+    }
+}
