7e9b80d14cff2250b7ae3bb404225e945be8a6fc
azenovich <alexander.zenovich@gmail.com>
2018-04-01 16:40:27 +0300

    HotelDTO was created. Hotel was initialized.

---
+++ NET.S.2018.Zenovich.08.Hotel.BLL/Services/HotelService.cs
@@ -0,0 +1,14 @@
+﻿using NET.S._2018.Zenovich._08.Hotel.BLL.API;
+using NET.S._2018.Zenovich._08.Hotel.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2018.Zenovich._08.Hotel.BLL.Services
+{
+    public class HotelService : IService<HotelDTO>
+    {
+    }
+}
